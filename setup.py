56a604090ae68366445004d98696f64b2d6267fa
miroslav14 <lukacmiroslav@yahoo.co.uk>
2019-09-23 03:13:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=readme,
     author='Jupyter Development Team',
     author_email='jupyter@googlegroups.com',
-    url='https://github.com/jupyter/echo_kernel',
+    url='https://github.com/miroslav14/qtpi_short.git',
     install_requires=[
         'jupyter_client', 'IPython', 'ipykernel'
     ],
